966403c38831aae2303128b35bc57aa40466a30c
Seung Tack Cho <seungtackcho@Seungui-MacBookAir.local>
2023-06-15 21:14:39 +0900

    [create]App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import { WeatherProvider } from './context/WeatherContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+const queryClient = new QueryClient();
+  it('render app', ()=> {
+    render(
+    <WeatherProvider>
+      <QueryClientProvider client={queryClient}>
+        <App/>
+      </QueryClientProvider>
+    </WeatherProvider>)
+  });
 });
+
+// 테스트 목록 
+// useQuery의 isLoading이 참 일때 로딩되는 지
+// 에러일떼 에러 표시 하는지
