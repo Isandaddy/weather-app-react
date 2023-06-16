6fd06fa638b1247ca2303fbc12560b72b52fd7d2
Seung Tack Cho <seungtackcho@Seungui-MacBookAir.local>
2023-06-16 16:04:48 +0900

    [create & move]test dir

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
-import { WeatherProvider } from './context/WeatherContext';
+import App from '../App';
+import { WeatherProvider } from '../context/WeatherContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 describe('App', () => {
@@ -15,7 +15,3 @@ const queryClient = new QueryClient();
     </WeatherProvider>)
   });
 });
-
-// 테스트 목록 
-// useQuery의 isLoading이 참 일때 로딩되는 지
-// 에러일떼 에러 표시 하는지
