c2944d8b4b51016deb5485ad68b5c055d2f7651a
Seung Tack Cho <seungtackcho@Seungui-MacBookAir.local>
2023-06-19 15:31:03 +0900

    [fix]App test 更新

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, renderHook, waitFor } from '@testing-library/react';
 import App from '../App';
 import { WeatherProvider } from '../context/WeatherContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 
 describe('App', () => {
-const queryClient = new QueryClient();
-  it('render app', ()=> {
-    render(
+  const queryClient = new QueryClient();
+
+  it('render app', async ()=> {
+    const {getByText} = render(
     <WeatherProvider>
       <QueryClientProvider client={queryClient}>
         <App/>
       </QueryClientProvider>
-    </WeatherProvider>)
+    </WeatherProvider>);
+
+    await waitFor(() => {
+      const dataElement = getByText('Sapporo');
+      expect(dataElement).toBeInTheDocument();
+    });
   });
+
 });
